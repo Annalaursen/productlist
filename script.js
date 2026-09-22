07ad53617fd2d06aa9356e44c9cf9813efaf7777
Anna Laursen <anla1007@stud.ek.dk>
2026-09-22 14:44:51 +0200

    js øvelser

---
+++ script.js
@@ -0,0 +1,21 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/products?limit=20";
+
+const produktliste = document.querySelector("section");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    produktliste.innerHTML += `
+<article class="card">
+<img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp alt=produktbillede />
+  <h2>${element.productdisplayname}</h2>
+  <h3>${element.brandname}</h3>
+  <p>${element.articletype}</p>
+  <p>${element.price}</p>
+  </article>`;
+  });
+}
